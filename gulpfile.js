fb542c2d52f33ffef136510a4e50523719bdcd18
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-09-03 10:33:42 +0430

    partials

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("style.scss").pipe(sass()).pipe(dest("styles"));
+  return src("*.scss").pipe(sass()).pipe(dest("styles"));
 }
 
 function watchTask() {
-  watch(["style.scss"], buildStyles);
+  watch(["*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
